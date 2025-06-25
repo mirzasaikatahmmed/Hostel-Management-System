89a61c143dbcf4fc56810edf3a4e99e122de940a
MIRZA SAIKAT AHMMED <mirzasaikatahmmed@gmail.com>
2025-06-25 10:15:28 +0600

    Add MyRoom form to Hostel Management System
    This commit introduces a new form called `MyRoom`, including its code-behind (`MyRoom.cs`), designer file (`MyRoom.Designer.cs`), and resource file (`MyRoom.resx`). The project file (`.csproj`) is updated to include these new files, ensuring they are compiled and embedded as resources. The `MyRoom` form is designed to display room details, featuring a refresh button and a data grid view for user room information.

---
+++ View/Rooms/MyRoom.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Hostel_Management_System.View.Rooms
+{
+    partial class MyRoom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.refreshBTN = new System.Windows.Forms.Button();
+            this.myRoomDataGridView = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.myRoomDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 23);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "My Room";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(702, 43);
+            this.panel1.TabIndex = 7;
+            // 
+            // refreshBTN
+            // 
+            this.refreshBTN.BackColor = System.Drawing.SystemColors.Highlight;
+            this.refreshBTN.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.refreshBTN.ForeColor = System.Drawing.Color.White;
+            this.refreshBTN.Location = new System.Drawing.Point(565, 49);
+            this.refreshBTN.Name = "refreshBTN";
+            this.refreshBTN.Size = new System.Drawing.Size(125, 41);
+            this.refreshBTN.TabIndex = 24;
+            this.refreshBTN.Text = "Refresh";
+            this.refreshBTN.UseVisualStyleBackColor = false;
+            // 
+            // myRoomDataGridView
+            // 
+            this.myRoomDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.myRoomDataGridView.Location = new System.Drawing.Point(14, 108);
+            this.myRoomDataGridView.Name = "myRoomDataGridView";
+            this.myRoomDataGridView.RowHeadersWidth = 51;
+            this.myRoomDataGridView.RowTemplate.Height = 24;
+            this.myRoomDataGridView.Size = new System.Drawing.Size(674, 418);
+            this.myRoomDataGridView.TabIndex = 29;
+            // 
+            // MyRoom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 538);
+            this.Controls.Add(this.myRoomDataGridView);
+            this.Controls.Add(this.refreshBTN);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MyRoom";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MyRoom";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.myRoomDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button refreshBTN;
+        private System.Windows.Forms.DataGridView myRoomDataGridView;
+    }
+}
---
+++ View/Rooms/MyRoom.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hostel_Management_System.View.Rooms
+{
+    public partial class MyRoom : Form
+    {
+        public MyRoom()
+        {
+            InitializeComponent();
+        }
+        public MyRoom(int studentID)
+        {
+            InitializeComponent();
+            LoadStudentDetails(studentID);
+            LoadRoomDetails(studentID);
+        }
+        public void LoadStudentDetails(int studentID)
+        {
+        }
+        public void LoadRoomDetails(int studentID)
+        {
+        }
+    }
+}
